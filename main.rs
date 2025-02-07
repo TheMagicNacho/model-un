976dbfb653bbfa96ce95c6a89b3944d17d25f228
Justin Peter <themagicnacho@protonmail.com>
2025-02-07 17:03:08 -0500

    stash work on rooms

---
+++ main.rs
@@ -0,0 +1,260 @@
+use futures::FutureExt;
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex, MutexGuard};
+use warp::ws::{Message, WebSocket};
+use warp::{Filter, Rejection, Reply};
+use futures::{SinkExt, StreamExt};
+use futures::stream::SplitSink;
+use tokio::sync::{broadcast, mpsc};
+use tokio::time::{Duration, Instant};
+use include_dir::{include_dir, Dir};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct PlayerState {
+    player_id: usize,
+    player_name: String,
+    value: Option<u8>,
+    // revealed: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct GameState {
+    players: Vec<PlayerState>,
+    all_revealed: bool,
+    notify_change: NotifyChange,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+#[derive(PartialEq)]
+struct NotifyChange {
+    current_id: usize,
+    new_id: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+enum ClientMessage {
+    ChangeValue { player_id: usize, value: u8 },
+    ChangeName    { player_id: usize, name: String },
+    RevealNumbers { value: bool },
+    Pong { player_id: usize },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+enum ServerMessage {
+    UpdateState(GameState),
+    PlayerAssigned { player_id: usize },
+    ErrorMessage(String),
+    Ping{data: usize},
+}
+
+type SharedGameState = Arc<Mutex<GameState>>;
+
+#[tokio::main]
+async fn main() {
+    let game_state = SharedGameState::new(Mutex::new(GameState {
+        players: Vec::new(),
+        all_revealed: false,
+        notify_change: NotifyChange::default(),
+    }));
+
+    let (tx, _rx) = broadcast::channel(32);
+    let game_state_filter = warp::any().map(move || game_state.clone());
+    let tx_filter = warp::any().map(move || tx.clone());
+
+    let ws_route = warp::path("ws")
+        .and(warp::ws())
+        .and(game_state_filter.clone())
+        .and(tx_filter.clone())
+        .and_then(handle_ws_connection);
+
+    let static_route = warp::fs::dir("./src/static");
+    let routes = ws_route
+      .or(static_route)
+      .with(warp::cors().allow_any_origin());
+
+    println!("Server running on localhost:3000/");
+    warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
+    // warp::serve(routes).run(([0, 0, 0, 0], 3000)).await;
+}
+
+async fn handle_ws_connection(
+    ws: warp::ws::Ws,
+    game_state: SharedGameState,
+    tx: broadcast::Sender<GameState>,
+) -> Result<impl Reply, Rejection> {
+    Ok(ws.on_upgrade(move |socket| client_connected(socket, game_state, tx)))
+}
+
+fn calculate_player_id(state: &GameState) -> usize {
+    // if the players array is 6, then the player id should be increment from 10
+    if state.players.len() >= 6 {
+        return 10 + state.players.len();
+    }
+
+    // Otherwise, find the lowest unused player ID
+    for i in 0..state.players.len() {
+        if state.players.iter().all(|p| p.player_id != i) {
+            return i;
+        }
+    }
+    state.players.len()
+}
+async fn client_connected(
+    websocket: WebSocket,
+    game_state: SharedGameState,
+    tx: broadcast::Sender<GameState>,
+) {
+    let (mut ws_tx, mut ws_rx) = websocket.split();
+    let mut rx = tx.subscribe();
+
+    // Assign a new player ID and add the player to the game state
+    let outgoing_id = {
+        let mut state = game_state.lock().unwrap();
+        let new_id = calculate_player_id(&state);
+        state.players.push(PlayerState {
+            player_id: new_id,
+            player_name: "Connecting...".to_string(),
+            value: None,
+            // revealed: false,
+            // missed_checkins: 0,
+        });
+        println!("New client connected: {:?}", new_id);
+        new_id
+    };
+
+    let msg = serde_json::to_string(&ServerMessage::PlayerAssigned { player_id: outgoing_id }).unwrap();
+    let _ = ws_tx.send(Message::text(msg)).await;
+    let game_state_clone = game_state.clone();
+    let _ = tx.send(game_state.lock().unwrap().clone());
+
+    let ws_task = tokio::spawn(async move {
+        // Regular WebSocket task
+        let mut interval = tokio::time::interval(Duration::from_secs(5)); // Ping every second
+
+        loop {
+            tokio::select! {
+                _ = interval.tick().fuse() => {
+                    let ping_message = serde_json::to_string(&ServerMessage::Ping { data: 0 }).unwrap();
+
+                    if ws_tx.send(Message::text(ping_message)).await.is_err() {
+                        break; // Exit loop if sending fails (client disconnected)
+                    }
+                },
+
+                Ok(game_state) = rx.recv().fuse() => {
+                    let serialized = serde_json::to_string(&ServerMessage::UpdateState(game_state)).unwrap();
+                    if ws_tx.send(Message::text(serialized)).await.is_err() {
+                        break;
+                    }
+                },
+                else => break,
+            }
+        }
+    });
+
+    let rx_task = tokio::spawn(async move {
+        // Listen for incoming WebSocket messages
+        while let Some(Ok(msg)) = ws_rx.next().await {
+            if let Ok(text) = msg.to_str() {
+                if let Ok(client_message) = serde_json::from_str::<ClientMessage>(text) {
+                    handle_client_message(client_message, &game_state_clone, &tx).await;
+                }
+            }
+        }
+    });
+
+    // Wait for either task to finish, ignoring shutdown ordering
+    let _ = tokio::join!(ws_task, rx_task);
+
+    // Clean up after client disconnection
+    // missed_checkins_task.abort(); // Stop the missed check-ins task
+    let mut state = game_state.lock().unwrap();
+
+    if let Some(index) = state.players.iter().position(|p| p.player_id == outgoing_id) {
+        state.players.remove(index);
+        println!("Player {} disconnected and removed.", outgoing_id);
+        let player_in_waiting = find_player_in_waiting(&mut state);
+
+        
+        match player_in_waiting {
+            Some(player_id) => {
+                // get promoting player
+                let promoting_player = state.players.iter().find(|p| p.player_id == player_id).unwrap().clone();
+                let promoted_player_index = state.players.iter().position(|p| p.player_id == player_id).unwrap();
+                // delete record of promoting player
+                state.players.remove(promoted_player_index);
+                // promote the player
+                state.players.push(PlayerState {
+                    player_id: outgoing_id,
+                    player_name: promoting_player.player_name.clone(),
+                    value: None,
+                });
+                state.notify_change = NotifyChange {
+                    current_id: player_id,
+                    new_id: outgoing_id,
+                };
+                println!("Notify Change: {:?}", state);
+            }
+            None => {
+                state.notify_change = NotifyChange {
+                    current_id: 0,
+                    new_id: 0,
+                };
+            }
+        }
+    }
+}
+
+fn find_player_in_waiting(state: &mut MutexGuard<GameState>) -> Option<usize> {
+    if state.players.len() >= 6 {
+        for player in &state.players {
+            if player.player_id > 10 {
+                return Some(player.player_id);
+            }
+        }
+    }
+    None
+}
+
+async fn handle_client_message(
+    message: ClientMessage,
+    game_state: &SharedGameState,
+    tx: &broadcast::Sender<GameState>,
+) {
+    println!("Client message: {:?}", message);
+    let mut state = game_state.lock().unwrap();
+
+    match message {
+        ClientMessage::Pong { player_id } => {
+            if let Some(player) = state.players.iter_mut().find(|p| p.player_id == player_id) {
+                // player.missed_checkins = 0;
+            }
+        }
+        ClientMessage::ChangeValue { player_id, value } => {
+            if let Some(player) = state.players.iter_mut().find(|p| p.player_id == player_id) {
+                player.value = Some(value);
+            }
+        }
+        ClientMessage::ChangeName {player_id, name} => {
+            if let Some(player) = state.players.iter_mut().find(|p| p.player_id == player_id) {
+                player.player_name = name;
+            }
+        }
+        ClientMessage::RevealNumbers { value } => {
+            // Only zero out the values if the user wants to reset and the previous state was reviealed.
+            if value == false && state.all_revealed == true {
+                for player in &mut state.players {
+                    if player.value.is_some() {
+                        player.value = Some(0);
+                    }
+                }
+            }
+            // Update the stateddddddd
+            state.all_revealed = value;
+        }
+    }
+
+    let _ = tx.send(state.clone());
+}
--- src/static/game.js
+++ src/static/game.js
@@ -12,7 +12,7 @@ class Game {
     const ws = await this.connect_to_server("ws://localhost:3000/ws");
 
     // Debaouncing is used for the field inputs to limit spamming the server.
-    const debounce_time = 1;
+    const debounce_time = 5;
     let debounce_timer;
 
     // Add event listener to name input
@@ -34,11 +34,24 @@ class Game {
     // Add event listener to reveal button
     const reveal_button = document.getElementById("reveal-button");
     reveal_button.addEventListener("click", () => {
+
       this.handle_reveal_button_click(this.local_state, ws);
     });
   }
 
   handle_reveal_button_click(local_state, ws) {
+
+    const active_players = [...this.server_state.players].sort((a, b) => {
+      const idA = a && typeof a === 'object' && a.hasOwnProperty('player_id') ? a.player_id : Infinity; // Handle missing player_id gracefully
+      const idB = b && typeof b === 'object' && b.hasOwnProperty('player_id') ? b.player_id : Infinity;
+      return idA - idB;
+    }).slice(0, 5);
+    const is_missing_votes = active_players.some(obj => obj && typeof obj === 'object' && obj.value < 1);
+
+
+
+
+
     if (this.server_state.all_revealed) {
       // Set the Select to default value
       document.getElementById("player_value").value = 0;
@@ -48,7 +61,16 @@ class Game {
     request.type = "RevealNumbers";
     request.value = !this.server_state.all_revealed;
     // console.log("Sending WebSocket message:", request);
-    ws.send(JSON.stringify(request));
+
+    if (is_missing_votes && request.value === true){
+      const user_confirmed = confirm("Some delegates are missing votes.\nAre you sure you want to reveal?");
+      if (user_confirmed){
+        ws.send(JSON.stringify(request));
+      }
+    } else {
+      ws.send(JSON.stringify(request));
+    }
+
   }
   handle_value_change(local_state, ws) {
     local_state.value = parseInt(document.getElementById("player_value").value);
@@ -83,10 +105,14 @@ class Game {
 
         if (temp_state.type === "UpdateState") {
           this.server_state = temp_state;
+
           if (this.local_state.player_id === temp_state.notify_change.current_id){
-            console.log("Previous Player ID:", this.local_state.player_id);
-            this.local_state.player_id = temp_state.notify_change.new_id;
-            console.log("Updated Player ID:", this.local_state.player_id);
+            if (temp_state.notify_change.current_id !== temp_state.notify_change.new_id){
+              console.log("Previous Player ID:", this.local_state.player_id);
+              this.local_state.player_id = temp_state.notify_change.new_id;
+              console.log("Updated Player ID:", this.local_state.player_id);
+              this.handle_name_change(this.local_state, ws);
+            }
           }
           this.update_dom_from_server_state();
           console.log("Updated Server State:", this.server_state);
@@ -109,13 +135,21 @@ class Game {
     });
   }
   update_dom_from_server_state() {
-    // Clear all player cards to set them as vacant first
+    // create a hashmap to quickly determin if a player's id now vacant.
+    const ids = new Set(this.server_state.players.map(p => p.player_id));
+
     for (let i = 0; i < 6; i++) {
       const player_card_id = "player" + i + "id";
       const player_card_element = document.getElementById(player_card_id);
-      player_card_element.classList.remove("player-ready");
+
 
       if (player_card_element) {
+        player_card_element.classList.remove("player-ready");
+
+        if (!ids.has(i)){
+          player_card_element.classList.add("player-vacant")
+        }
+
         // Reset the name and value for all cards
         const player_name_element = document.getElementById(
           "player" + i + "name",
@@ -124,7 +158,7 @@ class Game {
           "player" + i + "value",
         );
         if (player_name_element) {
-          player_name_element.innerHTML = `Player ${i}`;
+          player_name_element.innerHTML = `Delegate ${i}`;
         }
         if (player_value_element) {
           player_value_element.innerHTML = "?";
@@ -134,7 +168,7 @@ class Game {
 
     // Update the player cards based on server state
     for (const player of this.server_state.players) {
-      if (player.player_id !== null) {
+      if (player.player_id !== null ) {
         const player_card_id = "player" + player.player_id + "id";
         const player_name_id = "player" + player.player_id + "name";
         const player_value_id = "player" + player.player_id + "value";
@@ -164,18 +198,54 @@ class Game {
             player_value_element.innerHTML = "?";
           }
         }
+        // Update the reveal/reset button text
+        const reveal_button_element = document.getElementById("reveal-button");
+        if (reveal_button_element) {
+          if (this.server_state.all_revealed) {
+            reveal_button_element.innerHTML = "Reset";
+          } else {
+            reveal_button_element.innerHTML = "Reveal";
+          }
+        }
+      }
+      const control_area = document.getElementById("polymorphic-hud");
+      const value_input = document.getElementById("player_value");
+      const reveal_button = document.getElementById("reveal-button");
+      const nameInput = document.getElementById("player_name");
+      const value_label = document.querySelector('label[for="player_value"]');
+
+      if (this.local_state.player_id > 10 ){  // Spectator Mode
+
+        if (control_area) {
+          if (value_input) value_input.style.visibility = "hidden";
+          if (reveal_button) reveal_button.style.visibility = "hidden";
+          if (value_label) value_label.style.visibility = "hidden";
+
+          // Create and insert the Spectator Mode message
+          if (!document.getElementById("spectator-message")){
+            const spectator_message = document.createElement("div");
+            spectator_message.innerHTML = "" +
+                "<h2>Spectator Mode</h2>" +
+                "<p>All the delegate seats are taken.</p>" +
+                "<p>When a current delegate leaves, you will automatically take their seat. In the mean time, feel free to enter your name!</p>";
+
+            spectator_message.id = "spectator-message"; // Give it an ID if you need to manipulate it later.
+            control_area.appendChild(spectator_message);
+          }
+
+        }
+      } else {
+        if (value_input) value_input.style.visibility = "visible";
+        if (reveal_button) reveal_button.style.visibility = "visible";
+        if (value_label) value_label.style.visibility = "visible";
+
+        if (document.getElementById("spectator-message")){
+          document.getElementById("spectator-message").remove();
+        }
+
       }
     }
 
-    // Update the reveal/reset button text
-    const reveal_button_element = document.getElementById("reveal-button");
-    if (reveal_button_element) {
-      if (this.server_state.all_revealed) {
-        reveal_button_element.innerHTML = "Reset";
-      } else {
-        reveal_button_element.innerHTML = "Reveal";
-      }
-    }
   }
 }
 const game = new Game();
